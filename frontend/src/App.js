598074bb84586750968c1d3eef72c45ecd0bbc29
akshayyadav2005 <>
2024-09-05 14:38:41 +0530

    push
    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './Login';
+import Signup from './Signup'
 
 function App() {
   return (
     <div className="App">
-      Home Page
+       <Router>
+        <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+        </Routes>
+       </Router>
     </div>
   );
 }
